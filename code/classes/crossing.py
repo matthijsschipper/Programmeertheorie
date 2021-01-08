9fb8d3115ff3df2f909afa77da4fe08fdac73f21
Jade Leurs <jadeleurs@Jades-MacBook-Air-19543.local>
2021-01-08 11:04:56 +0100

    First version of Crossing Class

--- code/classes/crossing.py
+++ code/classes/crossing.py
@@ -1,3 +1,26 @@
 class Crossing():
-    def __init__():
-        pass
+    def __init__(self, x_coordinate, y_coordinate, z_coordinate):
+        self.location = (x_coordinate, y_coordinate, z_coordinate)
+        self.links = []
+        self.is_gate = False
+        self.name = none
+        
+    def place_gate(self, number):
+        """
+        Add gate to crossing.
+        """
+        self.is_gate = True
+        self.name = number
+    
+    def create_link(self, adjacent_crossing):
+        """
+        Create link between current and adjacent crossing.
+        """
+        paths.append((adjacent_crossingn))
+    
+    def get_links(self):
+        """
+        Returns list of linked crossings
+        """
+        return(self.links)
+
