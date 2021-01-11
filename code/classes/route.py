9658aed9011d74ac739970e94d32fbae166f125b
Jade Leurs <jadeleurs@Jades-MacBook-Air-19543.local>
2021-01-11 04:15:45 +0100

    improved add_step function

--- code/classes/route.py
+++ code/classes/route.py
@@ -7,13 +7,20 @@ class Route:
         self.add_step()
 
     def add_step(self):
-        for x in range(abs(int(self.start[0]) - int(self.end[0])) + 1):
-            self.routelist.append((x, int(self.start[1])))
-        
-        difference = int(self.start[1]) - int(self.end[1]) + 1
+        x_difference = self.start[0] - self.end[0]
 
-        if difference < 0:
-            for y in range(abs(difference)):
-                self.routelist.append((int(self.end[0]), int(self.start[1]) - y))
+        for x in range(abs(x_difference) + 1):
+            if x_difference < 0:
+                self.routelist.append((self.start[0] + x, self.start[1]))
+            if x_difference > 0:
+                self.routelist.append((self.start[0] - x, self.start[1]))
         
+        y_difference = self.start[1] - self.end[1]
+
+        for y in range(1, abs(y_difference) + 1):
+            if y_difference < 0:
+                self.routelist.append((self.end[0], self.start[1] + y))
+            if y_difference > 0:
+                self.routelist.append((self.end[0], self.start[1] - y))
+
         print(self.routelist)
