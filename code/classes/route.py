e3117e8265eca55732f24a33f535fce0579642fa
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-11 11:48:34 +0100

    Two comparisons for every iteration in the loop is no longer necessary with this construction

--- code/classes/route.py
+++ code/classes/route.py
@@ -12,15 +12,17 @@ class Route:
         for x in range(abs(x_difference) + 1):
             if x_difference < 0:
                 self.routelist.append((self.start[0] + x, self.start[1]))
-            if x_difference > 0:
-                self.routelist.append((self.start[0] - x, self.start[1]))
+                continue
+
+            self.routelist.append((self.start[0] - x, self.start[1]))
         
         y_difference = self.start[1] - self.end[1]
 
         for y in range(1, abs(y_difference) + 1):
             if y_difference < 0:
                 self.routelist.append((self.end[0], self.start[1] + y))
-            if y_difference > 0:
-                self.routelist.append((self.end[0], self.start[1] - y))
+                continue
+
+            self.routelist.append((self.end[0], self.start[1] - y))
 
         print(self.routelist)
