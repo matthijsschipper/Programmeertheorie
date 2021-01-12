f74b9b0e6b8f8988a8e3714c8102fbfcb3850263
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-12 14:00:31 +0100

    Added variable to check for dead ends

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -16,6 +16,7 @@ class Random():
             start_gate, end_gate = connection.start, connection.end
             self.current_location = start_gate.location
             self.end_location = end_gate.location
+            self.dead_end = False
 
             # Randomly walk over grid until current Crossing object is end_gate
             while self.current_location != self.end_location:
@@ -25,6 +26,7 @@ class Random():
                 # If no directions are available anymore, the random route has failed to reach the end gate
                 if self.current_crossing.get_possible_directions() == []:
                     print(f'The random route got stuck (no options left to choose from)!')
+                    self.dead_end = True
                     break
                 
                 self.directions = self.current_crossing.get_possible_directions()
@@ -62,6 +64,10 @@ class Random():
                     self.current_location[2] -= 1
                     continue
             
+            # Print statement is not applicable to routes with a dead end
+            if self.dead_end:
+                continue
+
             print(f'Amount of steps to get from {start_gate} to {end_gate} with random assigning directions is: {self.steps}.')
         
         self.grid.get_output()
