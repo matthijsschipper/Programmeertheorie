639ce7baf5796429cc92b5baba9ce8f059165b57
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-12 14:02:05 +0100

    My version of main.py, apperently need to commit it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from code.classes import grid, crossing, net
-from code.algorithms import random
 from code.visualisation import visualise as vis
 from code.algorithms import greedy as gr, random
 
@@ -7,8 +6,6 @@ if __name__ == "__main__":
     
     # create grid object
     grid = grid.Grid("./data/example/print_0.csv", "./data/example/netlist_1.csv")
-    grid.get_output()
 
-    # random = random.Random(grid)
-
-    # vis.visualise("./data/example/our_output.csv")
+    # Testing random algorithm
+    r = random.Random(grid)
