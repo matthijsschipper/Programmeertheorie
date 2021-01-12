cc21c2cf41db60719f5f98c898db7268d8347544
BirgitSchouten <birgitaaschouten@hotmail.com>
2021-01-12 17:17:53 +0100

    Have to take a break, bug in route deletion still there

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -24,19 +24,10 @@ class Random():
 
             # Randomly walk over grid until current Crossing object is end_gate
             while not self.grid.net_is_finished():
-                # Wss niet nodig
-                # x, y, z = self.current_location[0], self.current_location[1], self.current_location[2]
-                # self.current_crossing = self.grid.grid[z][y][x]
-
-                # If no directions are available anymore, the random route has failed to reach the end gate
-                # if self.current_crossing.get_possible_directions() == []:
-                #     print(f'The random route got stuck (no options left to choose from)!')
-                #     self.dead_end = True
-                #     break
                 
                 if self.grid.get_directions() == []:
-                    print(f'Dead end')
                     self.dead_end = True
+                    print(self.grid.current_net.show_route_coordinates())
                     self.grid.delete_net(net, -1)
                     break
 
@@ -44,40 +35,10 @@ class Random():
                 self.directions = self.grid.get_directions()
                 random_direction = random.choice(self.directions)
 
-                # Remove selected direction from available directions
-                # Moet nog aangepast worden
-                # self.current_crossing.add_blockade(random_direction)
-
-                # # Mark current crossing as visited
-                # self.current_crossing.set_visited()
-
-                # # Add current crossing to routelist
-                # connection.routelist.append(self.current_crossing)
-
                 self.grid.add_to_net(random_direction)
 
                 # Count steps
                 self.steps += 1
-
-                # Continue kan eruit
-                # if random_direction == 'N':
-                #     self.current_location[1] -= 1
-                #     continue
-                # elif random_direction == 'E':
-                #     self.current_location[0] += 1
-                #     continue
-                # elif random_direction == 'S':
-                #     self.current_location[1] += 1
-                #     continue
-                # elif random_direction == 'W':
-                #     self.current_location[0] -= 1
-                #     continue
-                # elif random_direction == 'U':
-                #     self.current_location[2] += 1
-                #     continue
-                # elif random_direction == 'D':
-                #     self.current_location[2] -= 1
-                #     continue
             
             # Print statement is not applicable to routes with a dead end
             if self.dead_end:
