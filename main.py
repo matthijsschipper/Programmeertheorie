98ee523c5bd4a0b7f3c7697db20724d89a6d0a69
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-13 18:23:05 +0100

    Recreated main.py, sorry

---
+++ main.py
@@ -0,0 +1,13 @@
+from code.classes import grid, crossing, net
+from code.visualisation import visualise as vis
+from code.algorithms import random
+
+if __name__ == "__main__":
+    
+    # create grid object
+    grid = grid.Grid("./data/example/print_0.csv", "./data/example/netlist_1.csv")
+
+    # Testing random algorithm
+    r = random.Random(grid)
+
+    vis.visualise("./data/example/print_0.csv", "./data/example/our_output.csv")
