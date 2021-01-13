635e303ca987975044d4d640b273b82869c70208
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-13 12:17:46 +0100

    Added comments, cleared up code

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -2,42 +2,55 @@ import copy
 import random
 
 class Random():
+    """
+    Randomly choose one of the available connections at the current crossing, starting
+    at self.start_gate, ending at self.end_gate. A chosen direction gets removed from
+    the available options, as well as the opposite direction from the new location.
+    """
     def  __init__(self, grid):
         self.grid = copy.deepcopy(grid)
         self.netlist = self.grid.available_nets()
         self.directions = None
+        self.start_gate = None
+        self.end_gate = None
         self.total_wires_length = 0
         self.total_costs = 0
-        self.check_netlist = False
+        
         self.random_routes(self.netlist)
     
     def random_routes(self, netlist):
+        """
+        Iterate over every Net object and start plotting a route from the start Crossing object.
+        """
         for net in netlist:
             self.grid.choose_net(net)
 
             self.steps = 0
-            start_gate, end_gate = net.start, net.end
-            self.current_location = start_gate.location
-            self.end_location = end_gate.location
+            self.start_gate = net.start
+            self.end_gate = net.end
             self.dead_end = False
-
-            # Randomly walk over grid until current Crossing object is end_gate
 
             """
             Wat er fout ging: self.grid.net_is_finished() aangeroepen -> roept current_net.is_finished() aan
             -> returned boolean -> loop luistert niet als boolean is -> response te sloom?
             """
+            # Net finished variables return True if crossing added to routelist is the destination
             while not net.finished:
 
+                # Empty directions list means a dead end
                 if self.grid.get_directions() == []:
                     print(f'Dead end from {start_gate} to {end_gate} ({self.steps} steps taken)')
                     self.dead_end = True
-                    # self.grid.delete_net(net, -1)
+                    
+                    # Delete dead end nets
+                    self.grid.delete_net(net, -1)
                     break
 
+                # Choose random direction ('N', 'E', 'S', 'W', 'U', 'D')
                 self.directions = self.grid.get_directions()
                 random_direction = random.choice(self.directions)
 
+                # Call add_to_net function
                 self.grid.add_to_net(random_direction)
 
                 # Count steps
@@ -49,17 +62,25 @@ class Random():
 
             print(f'Amount of steps to get from {start_gate} to {end_gate} with random assigning directions is: {self.steps}.')
         
+        self.check_netlist_implementation()
+        
     def check_netlist_implementation(self):
+        """
+        Check if any net is not finished. If true, stop. Else, move on to cost calculation.
+        """
         for net in self.grid.netlist:
             if not net.finished:
-                print('NOT FINISHED')
                 return False
-        
+              
         self.calculate_costs()
         return True
 
     
     def calculate_costs(self):
+        """
+        Costs are calculated by multiplying the total length of wires with 300 * the amount of intersections.
+        An intersection is where lines use the same crossing object, but have a different direction.
+        """
 
         for net in self.grid.netlist:
             self.total_wires_length += (net.get_length() - 1)
