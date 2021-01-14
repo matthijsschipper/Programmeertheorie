024082d9578f3455ff589dd15e7ad19291a3e28a
Jade Leurs <jadeleurs@Jades-MacBook-Air-19543.local>
2021-01-14 15:14:23 +0100

    Worked on A* algorithm. I thought I had already pushed this, but apparently something went wrong.

--- code/algorithms/astar.py
+++ code/algorithms/astar.py
@@ -1,5 +1,6 @@
 '''
 import copy
+from queue import PriorityQueue
 
 
 class Astar():
@@ -7,62 +8,104 @@ class Astar():
         self.grid = copy.deepcopy(grid)
         self.netlist = self.grid.available_nets()
 
-        self.get_routes(self.netlist)
+        self.run(self.netlist)
     
-    def get_routes(self):
+    def run(self, netlist):
         for net in netlist:
 
-            open_list = []
-            closed_list = []
+            # miss in methode in grid
+            start = net.start
+            end = net.end
 
-            self.grid.choose_net(net)
+            count = 0
+            open_set = PriorityQueue()
+            # f-score, count for breaking ties 
+            open_set.put((0, count, start))
+            previous = {}
 
-            open_list.append(self.grid.current_net.start)
-            
-            while len(open_list) != 0:
+            # pakt hij hier niet de diepte? Miss self.grid[1]
+            g_score = {crossing: float("inf") for row in self.grid for crossing in row}
+            g_score[start] = 0
 
-                index = 0
-                lowest_f = get_f(open_list[0])
+            f_score = {crossing: float("inf") for row in self.grid for crossing in row}
+            f_score[start] = h(start, end)
 
-                for i in range(len(open_list)):
+            # items in PriorityQueue
+            items = {start}
 
-                    f = get_f(open_list[i])
+            while not open_set.empty():
+                current = open_set.get()[2]
+                items.remove(current)
 
-                    if f < lowest_f:
-                        lowest_f = f
-                        index = i
-                
-                current = open_list.pop(index)
-                closed_list.append(current)
-
-                # path is found
-                if current == self.grid.current_net.end:
+                if current == end:
+                    # make path
                     return True
                 
-                directions = self.grid.get_directions()
+                for neighbor in current.neighbors:
+                    temp_g_score = g_score[current] + 1
 
-                for direction in directions:
-                    neighbour = self.grid.add_to_net(direction)
-
-                    if neighbour not in open_list:
-                        open_list.append(neighbour)
-                    
-                    self.grid.delete_last_crossing(self, net)
-
-
-
-    def get_f(self, crossing):
-        return get_g(crossing) + get_h(crossing)
-
-    def get_g(self, crossing):
-        # aantal afgelegde stappen
-        pass
+                    if temp_g_score < g_score[neighbor]:
+                        previous[neighbor] = current
+                        g_score[neighbor] = temp_g_score
+                        f_score[neighbor] = temp_g_score + h(neighbor.get_coordinates(), end.get_coordinates())
+                        if neighbor not in items:
+                            count += 1
+                            open_set.put((f_score[neighbor], count, neighbor))
+                            items.add(neighbor)
+        
+        return False
     
-    def get_h(self):
-        # minimale aantal stappen te gaan
-        pass
+
+                
+    def h(self, crossing, end_crossing):
+        self.current_crossing = crossing
+        # nog iets doen met current_net
+        return len(get_directions_to_end())
     
     def set_parent(self, crossing, parent):
         pass
+    
+    def get_path():
+        pass
 
+
+# CHANGES TO DATASTRUCTURE
+
+in Crossing class
+self.neighbors = []
+
+in Grid class
+
+get crossing(self, x, y, z):
+    return self.grid[z][y][x]
+
+set_neighbors(self, crossing):
+    directions = crossing.get_possible_directions()
+
+    for direction in crossing.get_possible_directions
+        # retrieve new crossing object at right location
+        current_coordinates = crossing.get_coordinates()
+
+        # if direction is north, y coordinate goes down by one (may seem weird, has to do with indexing)
+        if direction == 'N':
+            new_y_coordinate = current_coordinates[1] - 1
+            new_crossing = self.grid[current_coordinates[2]][new_y_coordinate][current_coordinates[0]]
+
+        # if direction is south, y coordinate goes up by one (may seem weird, has to do with indexing)
+        elif direction == 'S':
+            new_y_coordinate = current_coordinates[1] + 1
+            new_crossing = self.grid[current_coordinates[2]][new_y_coordinate][current_coordinates[0]]
+
+        # if direction is east, x coordinate goes up by one
+        elif direction == 'E':
+            new_x_coordinate = current_coordinates[0] + 1
+            new_crossing = self.grid[current_coordinates[2]][current_coordinates[1]][new_x_coordinate]
+            
+        # if direction is west, x coordinate goes down by one
+        elif direction == 'W':
+            new_x_coordinate = current_coordinates[0] - 1
+            new_crossing = self.grid[current_coordinates[2]][current_coordinates[1]][new_x_coordinate]     
+
+        crossing.neighbors.append(new_crossing)
 '''
+
