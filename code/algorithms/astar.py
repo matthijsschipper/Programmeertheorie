850340db4ce250637d9344fca416d6962ccb7c38
Jade Leurs <jadeleurs@Jades-MacBook-Air-19543.local>
2021-01-20 03:28:07 +0100

    Worked on cleaning up A* algorithm

--- code/algorithms/astar.py
+++ code/algorithms/astar.py
@@ -3,6 +3,12 @@ from queue import PriorityQueue
 
 
 class Astar():
+    """
+    Algorithm that finds the best path for each net based on a heuristic function.
+    The heuristic function is the traveled distance from the starting gate
+    plus the minimal distance to the end gate. (All distances are Manhattan distances.)
+    """
+    
     def __init__(self, grid):
         self.grid = copy.deepcopy(grid)
         self.netlist = self.grid.available_nets()
@@ -10,22 +16,14 @@ class Astar():
         self.run()
     
     def run(self):
-        c = 1
+        length = 0
 
         for net in self.netlist:
 
-            print(f"net: {c}")
-            c += 1
-
             self.grid.current_net = net
 
-            # miss in methode in grid
             start = net.start
             end = net.end
-
-            print(f"start: {start.location}")
-            print(f"end: {end.location}")
-            print("")
 
             count = 0
             open_set = PriorityQueue()
@@ -34,56 +32,32 @@ class Astar():
             open_set.put((0, count, start))
             previous = {}
 
-            # AANPASSEN voor 3d
             g_score = {crossing: float("inf") for layer in self.grid.grid for row in layer for crossing in row}
             g_score[start] = 0
 
             f_score = {crossing: float("inf") for layer in self.grid.grid for row in layer for crossing in row}
             f_score[start] = self.h_score(start)
-            #print(f"f_score start: {f_score[start]}")
 
             # items in PriorityQueue
             items = {start}
 
             while not open_set.empty():
                 current = open_set.get()[2]
-                print(f"current location: {current.location}")
                 items.remove(current)
 
                 if current == end:
                     path = self.reconstruct(current, previous, start)
-                    #self.net(path)
-                    
-                    # print(path) als je .location gebruikt in reconstruct
                     directions_to_end = self.get_directions_to_end(path)
-                    print(f"directions to end: {directions_to_end}")
+                    length += len(directions_to_end)
                     self.add_net(directions_to_end, net)
-                    
-                    print("jeej eind bereikt")
                     break
+                
+                directions = self.grid.get_directions(current)
                 
                 neighbors = []
 
-                # aanpassen -> zonder current crossing
-
-                self.grid.current_crossing = current
-
-                directions = self.grid.get_directions()
-
-                '''
-                # weghalen bij 3d en korter schrijven
-                if 'U' in directions:
-                    directions.remove('U')
-                '''
-                
-                print(f"directions: {directions}")
-
                 for direction in directions:
-                    neighbors.append(self.grid.crossing_at_direction(direction))
-                
-                # later weghalen
-                #for neighbor in neighbors:
-                #    print(f"neighbor: {neighbor.location}")
+                    neighbors.append(self.grid.crossing_at_direction(direction, current))
                 
                 temp_g_score = g_score[current] + 1
 
@@ -98,16 +72,22 @@ class Astar():
                             count += 1
                             open_set.put((f_score[neighbor], count, neighbor))
                             items.add(neighbor)
-                print("")
-            print("")
-            #break
         
-        total_costs = 300 * self.grid.amount_of_intersections + 200
-        self.grid.get_output(total_costs)
+        intersections = self.grid.amount_of_intersections
+
+        costs =  length + 300 * intersections
+
+        # Optional
+        print("")
+        print(f"Total length: {length}")
+        print(f"Amount of intersections: {intersections}")
+        print(f"Costs: {costs}")
+        print("")
+
+        self.grid.get_output(costs)
 
     def h_score(self, crossing):
         self.grid.current_crossing = crossing
-        #print(f"directions to end from {crossing.location}: {self.grid.current_net.get_route_to_end(crossing)}")
         return len(self.grid.get_directions_to_end())
     
     def reconstruct(self, current, previous, start):
@@ -127,9 +107,7 @@ class Astar():
 
         for i in range(len(path) - 1):
             current = path[i].location
-            print(f"current: {current}")
             next = path[i + 1].location
-            print(f"next: {next}")
 
             if next[0] - current[0] == 1:
                 directions.append('E')
@@ -144,15 +122,7 @@ class Astar():
             elif next[2] - current[2] == -1:
                 directions.append('D')
             
-            print(directions)
-            
         return directions
-    
-    #weghalen
-    def net(self, path):
-        for step in path:
-            self.grid.current_net.add_crossing(step)
-
     
     def add_net(self, directions, net):
         self.grid.current_crossing = net.start
