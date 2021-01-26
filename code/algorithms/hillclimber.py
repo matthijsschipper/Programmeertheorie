598eaf8162393cd28f32e8b4db3c31e5cbdb8398
Matthijs Schipper <matthijsschipper@live.nl>
2021-01-26 08:56:23 +0100

    Updated hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -47,16 +47,16 @@ class HillClimber():
             # Costs check
             self.check_solution(new_astar_solution)
         
+        # Write final solution to output file
         self.solution.grid.get_output(self.costs)
 
         print(f'''
         Convergence of {self.convergence_count} reached! The results:
         -------------------------------------------------------
-        Optimized chip {chip_number} and netlist {netlist_number}
+        Optimized netlist {netlist_number} from chip {chip_number}
         Optimized costs from {self.old_costs} to {self.costs}
         Optimized intersections from {self.old_intersections} to {self.solution.grid.amount_of_intersections}
         Optimized length from {self.old_length} to {self.solution.grid.netlist_length()}
-        Failed nets: {self.solution.failed_nets}
         ''')
 
         return self.solution
@@ -67,23 +67,23 @@ class HillClimber():
         if true. Solutions with equal costs overwrite the current solution as well, because
         this creates a new solution which might prevent too early convergence.
         """
-        # These costs are focussed on calculating costs of A* solution and is a temporary fix
-        # Might be useful to write a function for the grid class or the algorithm to calculate the costs
-        new_costs = new_solution.grid.netlist_length() + (300 * new_solution.grid.amount_of_intersections)
-        old_costs = self.costs
+        # new_costs = new_solution.grid.netlist_length() + (300 * new_solution.grid.amount_of_intersections)
+        
+        # Retrieving costs of new found solution
+        new_costs = new_solution.grid.calculate_costs()
 
         # Checking for convergence
-        if old_costs == new_costs:
+        if self.costs == new_costs:
             self.cost_occurence += 1
         else:
             self.cost_occurence = 0
 
         # No new route found
         if len(new_solution.grid.available_nets()) > 0:
-            return
+            return False
 
-        # If true, overwrite current solution with new solution
-        if new_costs <= old_costs:
+        # If true, overwrite current solution and costs with new solution and costs
+        if new_costs <= self.costs:
             self.solution = new_solution
             self.costs = new_costs
 
@@ -91,3 +91,7 @@ class HillClimber():
                 print(f"Found new costs of {self.costs}")
             elif self.cost_occurence % 50 == 0:
                 print(f"Found costs of {self.costs} for {self.cost_occurence} times now")
+        
+             return True
+        
+        return False
